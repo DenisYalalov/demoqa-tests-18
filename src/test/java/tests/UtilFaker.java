c7bafc29fc268eb0c5d135cb2dc61acd9b8da243
Denis Yalalov <denis.yalalov@edna.ru>
2023-03-23 16:04:45 +0300

    public dz

--- src/test/java/tests/UtilFaker.java
+++ src/test/java/tests/UtilFaker.java
@@ -7,52 +7,52 @@ import java.util.Map;
 import static java.lang.String.valueOf;
 
 public class UtilFaker {
-    public static Faker faker = new Faker();
-    public static String Subjects;
+    Faker faker = new Faker();
+    String Subjects;
 
-    static {
+    {
         Subjects = faker.options().option("English", "Physics", "Chemistry", "Computer Science",
                 "Commerce", "Accounting", "Economics");
     }
 
-    public static String Hobbies;
+    String Hobbies;
 
-    static {
+    {
         Hobbies = faker.options().option("Music", "Sports", "Reading");
     }
 
-    static String Name = faker.name().firstName();
-    static String LastName = faker.name().lastName();
+    String name = faker.name().firstName();
+    String lastName = faker.name().lastName();
 
-    static String Email = faker.internet().emailAddress();
+    String email = faker.internet().emailAddress();
 
-    static String Gender = faker.options().option("Male", "Female", "Other");
+    String gender = faker.options().option("Male", "Female", "Other");
 
 
-    static String userNumber = 8 + faker.phoneNumber().subscriberNumber(9);
+    String userNumber = 8 + faker.phoneNumber().subscriberNumber(9);
 
-    static String dayOfBirth = valueOf(faker.number().numberBetween(1, 28));
+    String dayOfBirth = valueOf(faker.number().numberBetween(1, 28));
 
-    static String monthOfBirth = faker.options().option("January", "February",
+    String monthOfBirth = faker.options().option("January", "February",
             "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
-    static String yearOfBirth = valueOf(faker.number().numberBetween(1950, 2000));
+    String yearOfBirth = valueOf(faker.number().numberBetween(1950, 2000));
 
-    static String PictureLocation = "1.jpeg";
+    String pictureLocation = "1.jpeg";
 
-    static String userAddress = faker.address().streetName();
+    String userAddress = faker.address().streetName();
 
-    static String userState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
+    String userState = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
 
 
-    static Map<String, String[]> mapStateWithCity = Map.of(
+    Map<String, String[]> mapStateWithCity = Map.of(
             "NCR", new String[]{"Gurgaon", "Noida"},
             "Uttar Pradesh", new String[]{"Agra", "Lucknow", "Merrut"},
             "Haryana", new String[]{"Karnal", "Panipat"},
             "Rajasthan", new String[]{"Jaipur", "Jaiselmer"}
     );
-    static String state = faker.options().option(mapStateWithCity.keySet().toArray()).toString();
-    static String userCity = faker.options().option(mapStateWithCity.get(state));
+    String state = faker.options().option(mapStateWithCity.keySet().toArray()).toString();
+    String userCity = faker.options().option(mapStateWithCity.get(state));
 
 
 }
