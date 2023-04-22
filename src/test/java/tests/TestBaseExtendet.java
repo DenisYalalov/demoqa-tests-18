b13dc16eb5c2aaee9d484d41fe89bed2aef66e9d
Denis Yalalov <denis.yalalov@edna.ru>
2023-04-22 15:08:13 +0300

    added Helper

--- src/test/java/tests/TestBaseExtendet.java
+++ src/test/java/tests/TestBaseExtendet.java
@@ -11,16 +11,18 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
+import static exmaples.helpers.LoggerHelper.logAndPassMyParameter;
+
 public class TestBaseExtendet {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = logAndPassMyParameter("browser_size", "1920х1080");
+        Configuration.baseUrl =logAndPassMyParameter ("baseurl","https://demoqa.com");
+        Configuration.browser = logAndPassMyParameter("browser", "chrome");
+        Configuration.browserVersion =logAndPassMyParameter ("browser_version","100.0");
         Configuration.pageLoadStrategy = "eager";
         Configuration.pollingInterval = 500;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = logAndPassMyParameter("selenoid_ur","https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
